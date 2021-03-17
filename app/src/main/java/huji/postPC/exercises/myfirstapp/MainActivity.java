6ee2633b4110dd47fc0801f26eb1f025b324a15e
Lior Kesten <t-liorkesten@microsoft.com>
2021-03-17 20:28:50 +0200

    Initial commit

---
+++ app/src/main/java/huji/postPC/exercises/myfirstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package huji.postPC.exercises.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
